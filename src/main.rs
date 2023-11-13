b94d1b0f4c87a37f41df891183ca371a50639967
Alessandro Caglio <acaglio@redhat.com>
2023-11-13 22:58:52 +0100

    Adding tracing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 mod handler;
 mod route;
 
-use tower_http::cors::{CorsLayer, Any};
+use std::time::Duration;
+
+use axum::{
+    body::{Body, BoxBody},
+    http::{Request, Response},
+};
+use tower_http::{
+    classify::ServerErrorsFailureClass,
+    cors::{Any, CorsLayer},
+    trace::TraceLayer,
+};
+use tracing::Span;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use uuid::Uuid;
 
 #[tokio::main]
 async fn main() {
@@ -10,13 +23,39 @@ async fn main() {
         .parse()
         .expect("Unable to parse env variable PORT");
 
+    let tracing_layer = TraceLayer::new_for_http()
+        .make_span_with(|_request: &Request<Body>| {
+            let request_id = Uuid::new_v4().to_string();
+            tracing::info_span!("http-request", %request_id)
+        })
+        .on_request(|request: &Request<Body>, _span: &Span| {
+            tracing::trace!("request: {} {}", request.method(), request.uri().path())
+        })
+        .on_response(
+            |response: &Response<BoxBody>, latency: Duration, _span: &Span| {
+                tracing::trace!("response: {} {:?}", response.status(), latency)
+            },
+        )
+        .on_failure(
+            |error: ServerErrorsFailureClass, _latency: Duration, _span: &Span| {
+                tracing::error!("error: {}", error)
+            },
+        );
+
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "api_service_axum=debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let cors = CorsLayer::new().allow_origin(Any);
 
-    let app = route::create_router().layer(cors);
+    let app = route::create_router().layer(cors).layer(tracing_layer);
 
     let addr = std::net::SocketAddr::from(([0, 0, 0, 0], http_port));
 
-    println!("Listening on {addr}");
+    tracing::warn!("Listening on {addr}");
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
