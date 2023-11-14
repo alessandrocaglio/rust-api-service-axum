a5913d07157716f57decbf09c434e0cb73f16ed1
Alessandro Caglio <acaglio@redhat.com>
2023-11-14 22:26:17 +0100

    Added simple http client to proxy a remote service

--- src/handler.rs
+++ src/handler.rs
@@ -1,4 +1,6 @@
-use axum::{http::StatusCode, response::IntoResponse, Json};
+use axum::{extract::Path, http::StatusCode, response::IntoResponse, Json};
+use hyper::Client;
+use hyper_tls::HttpsConnector;
 
 pub async fn health_check() -> impl IntoResponse {
     let json_response = serde_json::json!({
@@ -7,4 +9,20 @@ pub async fn health_check() -> impl IntoResponse {
     });
 
     (StatusCode::OK, Json(json_response))
+}
+
+pub async fn get_todos(Path(id): Path<u32>) -> impl IntoResponse {
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, hyper::Body>(https);
+
+    let uri = format!("https://jsonplaceholder.typicode.com/todos/{}", id)
+        .parse()
+        .unwrap();
+
+    let response = client.get(uri).await.unwrap();
+    let status_code = response.status();
+
+    let body_bytes = hyper::body::to_bytes(response.into_body()).await.unwrap();
+
+    (status_code, body_bytes)
 }
--- src/route.rs
+++ src/route.rs
@@ -1,4 +1,4 @@
-use crate::handler::health_check;
+use crate::handler::{get_todos, health_check};
 
 use axum::{routing::get, Router};
 
@@ -6,6 +6,7 @@ pub fn create_router() -> Router {
     Router::new()
         .route("/", get(|| async { "Hello, world!" }))
         .route("/health", get(health_check))
+        .route("/todos/:id", get(get_todos))
 }
 
 #[cfg(test)]
@@ -15,6 +16,25 @@ mod tests {
     use axum::http;
     use hyper::{Body, Request, StatusCode};
     use tower::util::ServiceExt;
+
+    #[tokio::test]
+    async fn test_handle_get_todo() {
+        let app = create_router();
+
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method(http::Method::GET)
+                    .uri("/todos/1")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+    }
+
     #[tokio::test]
     async fn test_handle_health_check() {
         let app = create_router();
